3e871ea9e2a08594a27052540eac6d8ff564f5e5
HienNguyen <ultsus0262@gmail.com>
2025-11-02 21:25:46 +0700

    Add LogoutController to handle user logout

--- src/java/controller/iam/LogoutController.java
+++ src/java/controller/iam/LogoutController.java
@@ -4,10 +4,39 @@
  */
 package controller.iam;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.io.IOException;
+
 /**
- *
+ * Handles user logout by invalidating the session and redirecting to login page
  * @author sonnt
  */
-public class LogoutController {
+@WebServlet(urlPatterns = "/logout")
+public class LogoutController extends HttpServlet {
     
+    protected void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        
+        if (session != null) {
+            session.invalidate();
+        }
+        HttpSession newSession = req.getSession(true);
+        newSession.setAttribute("message", "You have been successfully logged out.");
+        resp.sendRedirect(req.getContextPath() + "/login");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doProcess(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doProcess(req, resp);
+    }
 }
--- web/view/auth/login.jsp
+++ web/view/auth/login.jsp
@@ -4,15 +4,39 @@
     Author     : sonnt
 --%>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Login</title>
+        <style>
+            .alert {
+                padding: 10px;
+                margin-bottom: 15px;
+                border-radius: 4px;
+            }
+            .alert-success {
+                background-color: #d4edda;
+                color: #155724;
+                border: 1px solid #c3e6cb;
+            }
+            .alert-danger {
+                background-color: #f8d7da;
+                color: #721c24;
+                border: 1px solid #f5c6cb;
+            }
+        </style>
     </head>
     <body>
-         <form action="login" method="POST">
+        <c:if test="${not empty requestScope.message}">
+            <div }">
+                ${requestScope.message}
+            </div>
+        </c:if>
+        
+        <form action="login" method="POST">
             Username:<input type="text" name="username" id="txtUsername"/> <br/>
             Password:<input type="password" name="password" id="txtPassword"/><br/>
             <input type="submit" id="btnLogin" value="Login"/>
