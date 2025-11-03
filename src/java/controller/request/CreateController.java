ab6721677ff9a9e1d2e067cf9e86bb1e81e3b0f2
HienNguyen <ultsus0262@gmail.com>
2025-11-03 20:11:03 +0700

    Add CreateController servlet and create.jsp for create request page

--- src/java/controller/request/CreateController.java
+++ src/java/controller/request/CreateController.java
@@ -5,12 +5,21 @@
 package controller.request;
 
 import controller.iam.BaseRequiredAuthorizationController;
+import dal.DepartmentDBContext;
+import dal.RequestDBContext;
+import dal.RoleDBContext;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import model.Request;
+import model.iam.Role;
 import model.iam.User;
+import model.Department;
 
 /**
  *
@@ -21,10 +30,34 @@ public class CreateController extends BaseRequiredAuthorizationController {
 
     @Override
     protected void processPost(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
+        Request rq = new Request();
+        User cu = (User) req.getSession().getAttribute("auth");
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+        rq.setCreatedby(cu.getId());
+        rq.setCreatedtime(timestamp);
+        rq.setFrom(Date.valueOf(req.getParameter("fromDate")));
+        rq.setTo(Date.valueOf(req.getParameter("toDate")));
+        rq.setReason(req.getParameter("reason"));
+        rq.setStatus(0);
+
+        RequestDBContext db = new RequestDBContext();
+        db.insert(rq);
     }
 
     @Override
     protected void processGet(HttpServletRequest req, HttpServletResponse resp, User user) throws ServletException, IOException {
+        RoleDBContext rdb = new RoleDBContext();
+        DepartmentDBContext ddb = new DepartmentDBContext();
+        User cu = (User) req.getSession().getAttribute("auth");
+        Department dpt = ddb.get(cu.getId());
+        
+        ArrayList<Role> roles = rdb.getByUserId(cu.getId());
+        String dname = dpt.getName();
+        
+        req.setAttribute("dname", dname);
+        req.setAttribute("roles", roles);
+        req.getRequestDispatcher("/view/request/create.jsp").forward(req, resp);
     }
-    
+
 }
--- web/view/request/create.jsp
+++ web/view/request/create.jsp
@@ -4,16 +4,35 @@
     Author     : ultsu
 --%>
 
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Create leave request</title>
+        <title>JSP Page</title>
     </head>
     <body>
-        <form action="create" method="POST">
+        <h2>Create Leave Request</h2>
+        <form action="request/create" method="POST">
+            <label id="createdByLabel">Created by: ${sessionScope.auth.username}</label> <br>
+            <label>Role: 
+                <c:forEach items="${roles}" var="role" varStatus="status">
+                    ${role.name}<c:if test="${!status.last}">, </c:if>
+                </c:forEach>
+            </label> <br>
+
+            <label id="departmentLabel">Department: ${dname}</label> <br>
             
+            <label id="fromDateLabel">From (date):</label> 
+            <input type="date" name="fromDate" id="fromDate"> <br>
+            
+            <label id="toDateLabel">To (date):</label> 
+            <input type="date" name="toDate" id="toDate"> <br>
+            
+            <label id="reasonLabel">Reason:</label> <br>
+            <textarea id="reason" name="reason" rows="5" cols="50"></textarea> <br>
+            <input type="submit" value="Send" id="btnCreate">
         </form>
     </body>
 </html>
