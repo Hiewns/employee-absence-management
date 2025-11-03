d63d402d315f1ee6277952525763fb01263a4f70
HienNguyen <ultsus0262@gmail.com>
2025-11-03 19:09:59 +0700

    add RequestDBContext with insert method

---
+++ src/java/dal/RequestDBContext.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Request;
+
+/**
+ *
+ * @author ultsu
+ */
+public class RequestDBContext extends DBContext<Request>{
+
+    @Override
+    public ArrayList<Request> list() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Request get(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void insert(Request model) {
+        String sql = "INSERT INTO RequestForLeave([created_by], [created_time], [from], [to], reason, status) VALUES (?, ?, ?, ?, ?, ?)";
+        PreparedStatement stm = null;
+        try {
+            stm = connection.prepareStatement(sql, PreparedStatement.RETURN_GENERATED_KEYS);
+            stm.setInt(1, model.getCreatedby());
+            stm.setTimestamp(2, model.getCreatedtime());
+            stm.setDate(3, model.getFrom());
+            stm.setDate(4, model.getTo());
+            stm.setString(5, model.getReason());
+            stm.setInt(6, model.getStatus());
+            stm.executeUpdate();
+            
+            // Retrieve the auto-generated ID
+            ResultSet rs = stm.getGeneratedKeys();
+            if (rs.next()) {
+                int generatedId = rs.getInt(1);
+                model.setId(generatedId);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(RequestDBContext.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            closeConnection();
+        }
+    }
+
+    @Override
+    public void update(Request model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void delete(Request model) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
