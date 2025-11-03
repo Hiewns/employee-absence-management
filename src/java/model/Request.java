76ef27a95f8078c785de7f82e11d90dbd5faae32
HienNguyen <ultsus0262@gmail.com>
2025-11-03 18:56:17 +0700

    add model for Department and Request

---
+++ src/java/model/Request.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author ultsu
+ */
+public class Request extends BaseModel{
+    private int createdby;
+    private java.sql.Timestamp createdtime;
+    private java.sql.Date from;
+    private java.sql.Date to;
+    private String reason;
+    private int status;
+
+    public int getCreatedby() {
+        return createdby;
+    }
+
+    public void setCreatedby(int createdby) {
+        this.createdby = createdby;
+    }
+
+    public Timestamp getCreatedtime() {
+        return createdtime;
+    }
+
+    public void setCreatedtime(Timestamp createdtime) {
+        this.createdtime = createdtime;
+    }
+
+    public Date getFrom() {
+        return from;
+    }
+
+    public void setFrom(Date from) {
+        this.from = from;
+    }
+
+    public Date getTo() {
+        return to;
+    }
+
+    public void setTo(Date to) {
+        this.to = to;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+    
+}
